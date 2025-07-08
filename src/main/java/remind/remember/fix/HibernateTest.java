7985b2c93721de1c03b6d23e786d09c782c8f3f7
Plgdhd <aivanuk161@gmail.com>
2025-07-08 23:51:12 +0300

    hibernate 3

--- src/main/java/remind/remember/fix/HibernateTest.java
+++ src/main/java/remind/remember/fix/HibernateTest.java
@@ -1,9 +1,13 @@
 package remind.remember.fix;
 
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.dialect.lock.PessimisticEntityLockException;
 
+import remind.remember.fix.models.Person;
 import remind.remember.fix.models.PersonH;
 
 public class HibernateTest {
@@ -16,14 +20,14 @@ public class HibernateTest {
         Session session = sessionFactory.getCurrentSession();
         try {
             session.beginTransaction();
-            PersonH first = new PersonH("Oleg", 2);
-            PersonH second = new PersonH("Dimas", 24);
-            PersonH third = new PersonH("Bob", 12);
 
-            session.save(first);
-            session.save(second);
-            session.save(third);
-            
+            List<PersonH> people = session.createQuery("FROM Person WHERE age > 30 AND name LIKE 'T%'").getResultList();
+
+            session.createQuery("UPDATE Person SET name = 'Oleg WHERE age<20").executeUpdate();
+
+            for(PersonH data : people){
+                System.out.println(data);
+            }
             session.getTransaction().commit();
         } finally {
             sessionFactory.close();
